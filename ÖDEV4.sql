fa0c2b7a9ad5c94e7ce9a897b7cde2a3cca3bdc1
fadimecoşgun <102363800+ABCDSAVC@users.noreply.github.com>
2024-06-28 18:46:42 +0200

    Add files via upload

---
+++ ÖDEV4.sql
@@ -0,0 +1,6 @@
+-------- ödev4---------
+--1.select distinct replacement_cost from film;
+--2.select count(distinct replacement_cost) from film;
+--3.select count(*) from film where title LIKE 'T%' and rating='G';
+--4.select distinct country from country where length(country)=5;
+--5.select count(city) from city where city ILIKE '%r' ;
